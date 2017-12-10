e8a5713475885ec8c8933988d1ed906ab06386bf
Drew Martin <drew.e.martin@gmail.com>
2017-12-10 10:56:35 -0500

    Add files via upload

---
+++ test_header_items.ts
@@ -0,0 +1,67 @@
+let headerItems = [{
+    id: 1,
+    url: null,
+    parentId: 0,
+    title: 'headerItem1',
+    children: [{
+        id: 11,
+        url: 'https://www.google.com/search?q=11',
+        parentId: 1,
+        title: 'headerItem11',
+        children: []
+    },{
+        id: 12,
+        url: 'https://www.google.com/search?q=12',
+        parentId: 1,
+        title: 'headerItem12',
+        children: []
+    },{
+        id: 13,
+        url: null,
+        parentId: 1,
+        title: 'headerItem13',
+        children: [{
+            id: 113,
+            url: 'https://www.google.com/search?q=113 ',
+            parentId: 13,
+            title: 'headerItem113',
+            children: []
+        }]
+    }]
+},{
+    id: 2,
+    url: null,
+    parentId: 0,
+    title: 'headerItem2',
+    children: [{
+        id: 22,
+        url: 'https://www.google.com/search?q=22',
+        parentId: 2,
+        title: 'headerItem22',
+        children: []
+    },{
+        id: 24,
+        url: 'https://www.google.com/search?q=24',
+        parentId: 2,
+        title: 'headerItem24',
+        children: []
+    },{
+        id: 26,
+        url: null,
+        parentId: 2,
+        title: 'headerItem26',
+        children: [{
+            id: 226,
+            url: null,
+            parentId: 26,
+            title: 'headerItem226',
+            children: [{
+                id: 2226,
+                url: 'https://www.google.com/search?q=2226',
+                parentId: 226,
+                title: 'headerItem2226',
+                children: []
+            }]
+        }]
+    }]
+}];
